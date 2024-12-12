75a17b5038151c48e7c4d85d0dd15a92596e502a
Muhammed souban <muhammedsoubankunnummel123@gmail.com>
2024-12-12 13:30:25 +0530

    add: side bar

--- components/sidebar/Items.jsx
+++ components/sidebar/Items.jsx
@@ -1,16 +1,21 @@
-"use client";
+import Link from "next/link";
 import React, { useState } from "react";
 
-export default function Items({ name, icon }) {
-  const [selected, setSelected] = useState(false);
+export default function Items({ name, icon, onClick, selected, path}) {
   return (
-    <div className="border-l-2 border-x-blue-300 flex items-center justify-between w-full h-[40px] text-[14px] px-2 cursor-pointer text-white rounded-sm hover:border-zinc-700  hover:bg-zinc-700  ">
+   <Link href={`/${path}`}>
+    <div
+      className={` ${selected && 'bg-zinc-700'}  relative flex items-center justify-between w-full h-[40px] text-[14px] px-2 cursor-pointer text-white rounded-sm hover:border-zinc-700  hover:bg-zinc-700 `}
+      onClick={onClick}
+    >
       <div className="flex items-center gap-3">
         {icon} <p>{name}</p>
       </div>
       <div className="w-5 h-5 rounded-full bg-zinc-600 flex justify-center items-center text-[12px] ">
         3
       </div>
-    </div>
+
+     { selected && <div className="absolute left-0 w-1 rounded-2xl h-3 bg-sky-400"></div>}
+    </div></Link>
   );
 }
--- components/sidebar/SideBar.jsx
+++ components/sidebar/SideBar.jsx
@@ -1,29 +1,51 @@
-import React from 'react'
-import Items from './Items'
+"use client";
+import React, { useState } from "react";
+import Items from "./Items";
 import { IoSunnyOutline } from "react-icons/io5";
 import { FaRegStar } from "react-icons/fa6";
 import { TiTick } from "react-icons/ti";
+import { usePathname } from 'next/navigation'
+const sidbarItems = [
+    {
+      name: "My Day",
+      icon: <IoSunnyOutline className="text-xl" />,
+      path:"/",
+    },
+    {
+      name: "Important",
+      icon: <FaRegStar className="text-xl" />,
+      path:"important",
+    },
+    {
+      name: "Completed",
+      icon: <TiTick className="text-xl" />,
+      path:"completed",
+    },
+  ];
+export default function SideBar( ) {
+  const [selected, setSelected] = useState(sidbarItems[0].name);
+  const pathname = usePathname();
+  
 
-export default function SideBar() {
-    const sidbarItems = [
-        {
-            name:"My Day",
-            icon: <IoSunnyOutline className='text-xl'/>
-        },
-        {
-            name:"Important",
-            icon: <FaRegStar className='text-xl'/>
-        },
-        {
-            name:"Completed",
-            icon: <TiTick className='text-xl'/>
-        },
-    ]
+ 
+  const handleClick = (name) => {
+    setSelected(name);
+  };
+
   return (
-    <div className='w-[200px] h-screen bg-zinc-800 flex flex-col gap-2 pt-10 p-2'>
-       {sidbarItems.map((item)=>{
-           return <Items key={item.name} name={item.name} icon={item.icon}/>
-       })}
+    <div className="w-[200px] h-screen bg-zinc-800 flex flex-col gap-2 pt-10 p-2">
+      {sidbarItems.map((item) => {
+        return (
+          <Items
+            key={item.name}
+            name={item.name}
+            icon={item.icon}
+            onClick={() => handleClick(item.name)}
+            selected={selected === item.name }
+            path={item.path}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
